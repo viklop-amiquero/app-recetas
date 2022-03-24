bce162395ee9a0caed07c0bebae4545dbfcaf0ac
viklop-amiquero <69984938+viklop-amiquero@users.noreply.github.com>
2022-03-23 19:22:41 -0500

    septimo commit

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -4,8 +4,42 @@
     {{-- <h1>{{$receta}}</h1> --}}
     <article class="contenido-receta">
         
-        <h1 class="text-center mb-4"></h1>
+        <h1 class="text-center mb-4">{{$receta->titulo}}</h1>
 
+        <div class="imagen-receta container">
+            <img src="/storage/{{$receta->imagen}}" class = "w-100" alt="">
+        </div>
+
+        <div class="receta-meta container mt-2">
+            
+            <p>
+                <span class="font-weight-bold text-primary">Escrito en:</span>
+                {{$receta->categoria->nombre}}
+            </p>
+
+            <p>
+                <span class="font-weight-bold text-primary">Autor:</span>
+                {{-- TODO: Mostrar el usuario --}}
+                {{$receta->user_id}}
+            </p>
+
+            <p>
+                <span class="font-weight-bold text-primary">Fecha:</span>
+                {{-- TODO: Mostrar el usuario --}}
+                {{$receta->created_at}}
+            </p>
+
+            <div class="ingredientes">
+                <h2 class="my-3 text-primary">Ingredientes</h2>
+                {!!$receta->ingredientes!!}
+            </div>
+
+            <div class="preparacion">
+                <h2 class="my-3 text-primary">Preparación</h2>
+                {!!$receta->preparacion!!}
+            </div>
+
+        </div>
     </article>
 
 
